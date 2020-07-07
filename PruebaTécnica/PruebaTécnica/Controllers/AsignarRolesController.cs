6e496b10eb4e8b2f9f0750a43840a369b09205d3
Kris0296 <39197328+Kris0296@users.noreply.github.com>
2020-07-06 23:56:02 -0600

    sgd

---
+++ PruebaTécnica/PruebaTécnica/Controllers/AsignarRolesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PruebaTécnica.Modelo;
+
+namespace PruebaTécnica.Controllers
+{
+    public class AsignarRolesController : Controller
+    {
+        private DBPruebaEntities1 db = new DBPruebaEntities1();
+
+        // GET: AsignarRoles
+        public ActionResult Index()
+        {
+            var uSERROL = db.USERROL.Include(u => u.ROLES).Include(u => u.USUARIOS);
+            return View(uSERROL.ToList());
+        }
+
+        // GET: AsignarRoles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERROL uSERROL = db.USERROL.Find(id);
+            if (uSERROL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERROL);
+        }
+
+        // GET: AsignarRoles/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDRol = new SelectList(db.ROLES, "ID", "Nombre");
+            ViewBag.IDUser = new SelectList(db.USUARIOS, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: AsignarRoles/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,IDUser,IDRol")] USERROL uSERROL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USERROL.Add(uSERROL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDRol = new SelectList(db.ROLES, "ID", "Nombre", uSERROL.IDRol);
+            ViewBag.IDUser = new SelectList(db.USUARIOS, "ID", "Nombre", uSERROL.IDUser);
+            return View(uSERROL);
+        }
+
+        // GET: AsignarRoles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERROL uSERROL = db.USERROL.Find(id);
+            if (uSERROL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDRol = new SelectList(db.ROLES, "ID", "Nombre", uSERROL.IDRol);
+            ViewBag.IDUser = new SelectList(db.USUARIOS, "ID", "Nombre", uSERROL.IDUser);
+            return View(uSERROL);
+        }
+
+        // POST: AsignarRoles/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,IDUser,IDRol")] USERROL uSERROL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSERROL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDRol = new SelectList(db.ROLES, "ID", "Nombre", uSERROL.IDRol);
+            ViewBag.IDUser = new SelectList(db.USUARIOS, "ID", "Nombre", uSERROL.IDUser);
+            return View(uSERROL);
+        }
+
+        // GET: AsignarRoles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERROL uSERROL = db.USERROL.Find(id);
+            if (uSERROL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERROL);
+        }
+
+        // POST: AsignarRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USERROL uSERROL = db.USERROL.Find(id);
+            db.USERROL.Remove(uSERROL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PruebaTécnica/PruebaTécnica/Controllers/RolesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PruebaTécnica.Modelo;
+
+namespace PruebaTécnica.Controllers
+{
+    public class RolesController : Controller
+    {
+        private DBPruebaEntities1 db = new DBPruebaEntities1();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View(db.ROLES.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROLES rOLES = db.ROLES.Find(id);
+            if (rOLES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOLES);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre")] ROLES rOLES)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ROLES.Add(rOLES);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rOLES);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROLES rOLES = db.ROLES.Find(id);
+            if (rOLES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOLES);
+        }
+
+        // POST: Roles/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre")] ROLES rOLES)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rOLES).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rOLES);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROLES rOLES = db.ROLES.Find(id);
+            if (rOLES == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOLES);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ROLES rOLES = db.ROLES.Find(id);
+            db.ROLES.Remove(rOLES);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
